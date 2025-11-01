db142a85072d22b5e9dffb5d20694e047a529b37
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-11-01 09:15:24 -0300

    update: database schema

--- src/controllers/technology.controller.ts
+++ src/controllers/technology.controller.ts
@@ -1,8 +1,9 @@
 import type { FastifyInstance } from 'fastify';
-import { createTechnology } from './technology/create.js';
+import { createTechnology, type Technology } from './technology/create.js';
 import { listTechnology } from './technology/list.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 export const technologiesRoutes = (app: FastifyInstance) => {
-  app.post('/technologies', createTechnology);
+  app.post<{ Body: Technology }>('/technologies', { preHandler: authMiddleware }, createTechnology);
   app.get('/technologies', listTechnology);
 };
